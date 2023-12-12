fe3ee1258d4b0e48255b91b3ad046076a3778d25
Grant <you@example.com>
2023-12-12 13:28:07 +0300

    Migrition added and database created

---
+++ ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelProject.DataAccessLayer.Concrete
+{
+	public class Context : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(@"server=WINDOWSXP\MSSQLSERVER01;
+			initial catalog=ApiDb;integrated security=true;TrustServerCertificate=True");
+		}
+		// sol sınıf ismi sag tablo ismi
+		public DbSet<Room> Rooms { get; set; }
+		public DbSet<Service> Services { get; set; }
+		public DbSet<Staff> Staffs { get; set; }
+		public DbSet<Subscribe> Subscribes { get; set; }
+		public DbSet<Testimonial> Testimonials { get; set; }
+	}
+}
