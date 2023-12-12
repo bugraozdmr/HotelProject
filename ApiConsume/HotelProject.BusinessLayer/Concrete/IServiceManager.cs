43ade7122ef7cee5cb36471abe6ffe054ce62897
Grant <you@example.com>
2023-12-12 23:55:31 +0300

    Managers done

---
+++ ApiConsume/HotelProject.BusinessLayer/Concrete/IServiceManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+	public class IServiceManager : IServiceService
+	{
+		private readonly IServicesDal _servicesDal;
+
+		public IServiceManager(IServicesDal servicesDal)
+		{
+			_servicesDal = servicesDal;
+		}
+
+		public void TInsert(Service entity)
+		{
+			_servicesDal.Insert(entity);
+		}
+
+		public void TDelete(Service entity)
+		{
+			_servicesDal.Delete(entity);
+		}
+
+		public void TUpdate(Service entity)
+		{
+			_servicesDal.Update(entity);
+		}
+
+		public List<Service> TGetList()
+		{
+			return _servicesDal.GetList();
+		}
+
+		public Service TGetById(int id)
+		{
+			return _servicesDal.GetById(id);
+		}
+	}
+}
