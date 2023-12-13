8abf6189975eaee10415b39612b05194d674d0f5
Grant <you@example.com>
2023-12-13 14:23:18 +0300

    added

---
+++ Frontend/HotelProject.WebUI/Controllers/StaffController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class StaffController : Controller
+    {
+        [Route("/staff/index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
