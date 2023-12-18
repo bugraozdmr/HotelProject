dc21913862481bca9cc9f84428892bd9538768f5
Grant <you@example.com>
2023-12-18 23:40:19 +0300

    Partials and components

---
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_RoomPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.ViewComponents.Default
+{
+    public class _RoomPartial : ViewComponent
+    {   // invoke olmak zorunda yoksa patlıyor
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
