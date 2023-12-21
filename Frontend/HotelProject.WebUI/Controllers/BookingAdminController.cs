b5a821e0a95f1e20ee462fc30075b1a63a9d2479
Grant <you@example.com>
2023-12-21 12:40:35 +0300

    Booking admin controller index.cshtml

---
+++ Frontend/HotelProject.WebUI/Controllers/BookingAdminController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using HotelProject.WebUI.Dtos.BookingDto;
+
+namespace HotelProject.WebUI.Controllers
+{
+	public class BookingAdminController : Controller
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public BookingAdminController(IHttpClientFactory client)
+		{
+			_httpClientFactory = client;
+		}
+		public async Task<IActionResult> Index()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("http://localhost:5051/api/Booking");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultBookingDto>>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
+	}
+}
