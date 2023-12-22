dbb3c29c56e3145722c3156f2495619929ba926b
Grant <you@example.com>
2023-12-22 17:51:42 +0300

    few apis added

--- RapidApi/RapidApiConsume/Controllers/ConverterController.cs
+++ RapidApi/RapidApiConsume/Controllers/ConverterController.cs
@@ -1,36 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using RapidApiConsume.Models.Imdb;
+using RapidApiConsume.Models.Converter;
 
 namespace RapidApiConsume.Controllers
 {
-	public class ImdbController : Controller
+	public class ConverterController : Controller
 	{
 		public async Task<IActionResult> Index()
 		{
-			List<ImdbViewModel> list = new List<ImdbViewModel>();
-
 			var client = new HttpClient();
 			var request = new HttpRequestMessage
 			{
 				Method = HttpMethod.Get,
-				RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+				RequestUri = new Uri("https://apidojo-booking-v1.p.rapidapi.com/currency/get-exchange-rates?base_currency=USD&languagecode=en-us"),
 				Headers =
 				{
 					{ "X-RapidAPI-Key", "cc43fab76emsh21b71a713c00a15p1188cbjsn49cedfeefe88" },
-					{ "X-RapidAPI-Host", "imdb-top-100-movies.p.rapidapi.com" },
+					{ "X-RapidAPI-Host", "apidojo-booking-v1.p.rapidapi.com" },
 				},
 			};
 			using (var response = await client.SendAsync(request))
 			{
 				response.EnsureSuccessStatusCode();
 				var body = await response.Content.ReadAsStringAsync();
-				list = JsonConvert.DeserializeObject<List<ImdbViewModel>>(body);
 
-				return View(list);
+				var viewModel = JsonConvert.DeserializeObject<ConverterViewModel>(body);
+				return View(viewModel.exchange_rates.ToList());
 			}
-
-			return View();
 		}
 	}
 }
---
+++ RapidApi/RapidApiConsume/Models/Converter/ConverterViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace RapidApiConsume.Models.Converter
+{
+	public class ConverterViewModel
+	{
+		// edit - paste as special -- json
+
+		public Exchange_Rates[] exchange_rates { get; set; }
+		public string base_currency { get; set; }
+		public string base_currency_date { get; set; }
+	
+		// 3 tane deger var asıl lazım olan bunun altındakiler
+		public class Exchange_Rates
+		{
+			public string exchange_rate_buy { get; set; }
+			public string currency { get; set; }
+		}
+
+	}
+}
